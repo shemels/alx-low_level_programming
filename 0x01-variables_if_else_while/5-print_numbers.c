11479fd7bf5c26f69d3d94fd85bc02a6a53437c9
Shemels <shemels779@gmail.com>
2022-04-09 15:11:50 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int digit;
 
-for (digit =0; digit < 10; digit++)
+for (digit = 0; digit < 10; digit++)
 {
 printf("%digit", digit);
 }
@@ -17,6 +17,6 @@ printf("%digit", digit);
 printf("\n");
 
 return (0);
-
 }
+
  
