a80f0e3bf2a39c3b914e8bf2b31afeca997e0981
Shemels <shemels779@gmail.com>
2022-04-09 15:59:55 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar (i);
+}
+  
+putchar ('\n');
+
+return (0);
+
+}
+  
