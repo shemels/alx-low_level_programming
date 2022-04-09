a9fe94bd85fec207df5a4ad3d82ffbf295de2876
Shemels <shemels779@gmail.com>
2022-04-09 13:53:37 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0 (succes/correct)
+ */
+
+int main(viod)
+
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+contnue;
+      
+else if (alphabet == 'e')
+continue;
+
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
