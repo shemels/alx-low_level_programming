e35bff68a674be2242a892ddf2d97f7019966fc6
Shemels <shemels779@gmail.com>
2022-04-09 14:53:50 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet = 'a'; alpabet <= 'z'; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 if (alphabet == 'q')
 continue;
@@ -21,7 +21,7 @@ continue;
 putchar(alphabet);
 }
 
-putchar('\n')
+ putchar('\n');
 
 return (0);
 
