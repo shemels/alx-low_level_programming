0bfffc88472f4d3d0d0c970d550ddeb5ef89268a
Shemels <shemels779@gmail.com>
2022-04-09 16:13:23 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
  * main - entry point
  *
  * return: always 0
- * Description: success or right
+ * Description: prints all single numbers of base 10 starting from 0
  */
 int main(void)
 
@@ -13,11 +13,10 @@ int i;
 for (i = 48; i <= 57; i++)
 {
 putchar (i);
-}  
+}
 putchar ('\n');
 
 return (0);
 
 }
 
-  
