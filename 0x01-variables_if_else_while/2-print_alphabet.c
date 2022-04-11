0d17388a420bbf73c79287b4d49d2e95a16addd0
Shemels <shemels779@gmail.com>
2022-04-11 01:06:49 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,23 +2,20 @@
 /**
  * main - entry point
  *
- * return: always 0
+ *Return:Always 0
  * Description: success or correct
  */
 int main(void)
 
 {
 char lower_case;
-char upper_case;
-
+	
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 }
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-{
-putchar(upper_case);
-}
+
+putchar('\n');
 
 return (0);
 
