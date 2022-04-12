2d96d7f541e0f052a1d18dcebd241194fa560b41
Shemels <shemels779@gmail.com>
2022-04-12 16:22:19 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,8 +27,8 @@ _putchar(' ');
 
 if (prod >= 100)
 {
-_putchar((prod / 100) + '0'));
-_putchar(((prod / 10) % 10 + '0');
+_putchar((prod / 100) + '0');
+_putchar((prod / 10) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
 {
@@ -36,6 +36,7 @@ _putchar((prod / 10) + '0');
 _putchar((prod % 10) + '0');
 }
 _putchar('\n');
+}
 }
 }
 }
