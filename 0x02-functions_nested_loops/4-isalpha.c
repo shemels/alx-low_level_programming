79126afa71e02690a5c2919995cadf50bfb746f7
Shemels <shemels779@gmail.com>
2022-04-12 12:03:48 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  */
 int _isalpha(int c)
 {
-  if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 {
 return (1);
 }
@@ -15,4 +15,3 @@ else
 return (0);
 }
 }
-    
