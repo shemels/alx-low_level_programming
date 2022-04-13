ff2bfec1d46740ccbc0a2b998c5246df1eba8792
Shemels <shemels779@gmail.com>
2022-04-12 23:35:17 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ int num, mult, prod;
 
 if (n >= 0 && n <= 15)
 {
-for (num = 0; num < n; num++)
+for (num = 0; num <= n; num++)
 {
 _putchar('0');
 
@@ -28,7 +28,7 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + '0');
-_putchar((prod / 10) % 10 + '0');
+_putchar(((prod / 10)) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
 {
