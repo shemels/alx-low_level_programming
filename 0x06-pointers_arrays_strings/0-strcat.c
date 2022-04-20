4ff47b5bd4454582c52b784ed94989ac52492713
shemels <shemels779@gmail.com>
2022-04-20 10:22:50 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,21 +9,21 @@
 char *_strcat(char *dest, char *src)
 {
 
-  int i = 0, c = 0, n = 0;
+int i = 0, c = 0, n = 0;
 
-  while (dest[i] != '\0')
-    {
-      i++;
-    }
-  while (src[c] != '\0')
-    {
-      c++
-	}
-  while (n <= c)
-    {
-      dest[i] = src[n];
-      n++;
-      i++;
-    }
-  return (dest);
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[c] != '\0')
+{
+c++;
+}
+while (n <= c)
+{
+dest[i] = src[n];
+n++;
+i++;
+}
+return (dest);
 }
