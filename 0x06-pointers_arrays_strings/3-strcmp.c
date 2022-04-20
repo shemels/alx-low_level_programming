49eb486ac5ff0c4747448e1386f65721dd7c3c5b
shemels <shemels779@gmail.com>
2022-04-20 10:55:16 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-  int i = 0;
+int i = 0;
 
-  while ((s1[i] == s2[i] && (s1[i] != '\0'))
-    {
-      i++;
-    }
-    return (s1[i] - s2[i]);
-    }
+while ((s1[i] == s2[i] && (s1[i] != '\0'))
+{
+i++;
+}
+return (s1[i] - s2[i]);
+}
+
