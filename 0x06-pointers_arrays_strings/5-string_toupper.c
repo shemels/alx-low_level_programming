6436b4b04048326b65f58e84773601c7133e0bf1
shemels <shemels779@gmail.com>
2022-04-20 11:09:43 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper - function that changes lower case to upper case.
+ * string_toupper - Function that reverses the content of an array of integers.
  *
  * @s: s is the array
  *
@@ -9,15 +9,15 @@
  */
 char *string_toupper(char *s)
 {
-  int i = 0;
+	int i = 0;
 
-  while (s[i] != '\0')
-    {
-      if ((s[i] >= 97) && (s[i] <= 122))
+	while (s[i] != '\0')
 	{
-	  s[i] = s[i] - 32;
+		if ((s[i] >= 97) && (s[i] <= 122))
+		{
+			s[i] = s[i] - 32;
+		}
+	i++;
 	}
-      i++;
-    }
-  return (s);
+	return (s);
 }
