434fc881c9aafede0045ca9d956aa7c534ffb1cf
shemels <shemels779@gmail.com>
2022-04-20 11:12:51 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,33 @@
 #include "main.h"
 
 /**
- * cap_string - function that reverses the contents of an array of integers.
- * 
+ * cap_string - Function that reverses the content of an array of integers.
+ *
  * @s: s is the array
  *
  * Return: Always 0.
  */
-char *cup_string(char *s)
+char *cap_string(char *s)
 {
-  int i = 0;
+	int i = 0;
 
-  while (s[i] != '\0')
-    {
-      if (s[0] != 122 && s[0] >= 97)
+	while (s[i] != '\0')
 	{
-	  s[0] = s[0] - 32;
+		if (s[0] <= 122 && s[0] >= 97)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' ||
+			s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
+				s[i] == '!' || s[i] == '?' || s[i] == '(' ||
+				s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	i++;
 	}
-      if (s[i] == 32 || s[i] == 46 || s[i] == '\t' || s[i] == '\n\ || s[i] == 44 || s[i] == 59 ||
-	  s[i] == '!' || s[i] == '?' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}'
-	{
-	  if (s[i + 1] <= 122 && s[i +1] >= 97)
-	    {
-	      s[i + 1] = s[i + ] - 32;
-	    }
-	}
-	  i++;
-	  }
-      return (s);
-    }
+	return (s);
+}
