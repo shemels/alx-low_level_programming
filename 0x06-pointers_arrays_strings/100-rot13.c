6236b0cc6683dbca0f98a5da3538fd2c499f9836
shemels <shemels779@gmail.com>
2022-04-21 10:04:40 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rot13 - function that reverses the content of an array of integers.
+ * rot13 - Function that reverses the content of an array of integers.
  *
  * @str: s is the array
  *
@@ -9,19 +9,19 @@
  */
 char *rot13(char *str)
 {
-  int i = 0, k;
-  char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0, k;
+	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-  for (; str[i] != '\0'; i++)
-    {
-      for (k = 0; k <= 51; k++)
+	for (; str[i] != '\0'; i++)
 	{
-	  if (s[k] == str[i])
-	    {
-	      str[i] = s1[k];
-	    }
+		for (k = 0; k <= 51; k++)
+		{
+			if (s[k] == str[i])
+			{
+				str[i] = s1[k];
+			}
+		}
 	}
-    }
-  return (str);
+	return (str);
 }
